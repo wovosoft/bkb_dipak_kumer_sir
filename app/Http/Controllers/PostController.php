3a40c328b9f44c5b43cc35992c88894622d1b2ee
Narayan Adhikary <wovosoft@gmail.com>
2021-04-13 16:14:14 +0600

    updated

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,7 @@ class PostController extends Controller
 {
     public function index(Request $request)
     {
+//        if (auth()->user()->can('is_branch'))
         return view('blog', [
             "posts" => Post::query()
                 ->select(['*'])
